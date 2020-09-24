3abaebbaf9152ad70f7eaa72ad16e41215e70ed3
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-09-24 14:09:30 +0300

    Added MANIFEST.in file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     ext_modules=cythonize(["SentiNet/*.pyx", "SentiNet/*.pxd"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-SentiNet-Cy',
-    version='1.0.0',
+    version='1.0.1',
     packages=['SentiNet'],
     url='https://github.com/olcaytaner/TurkishSentiNet-Cy',
     license='',
